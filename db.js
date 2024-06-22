879b21f75999f7fe0dd909dc09591588cea25449
bpalazzi512 <60822022+bpalazzi512@users.noreply.github.com>
2024-06-21 18:39:05 -0700

    first commit

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
+require("dotenv").config()
 const mysql = require('mysql')
 
 
 let db = mysql.createConnection({
-    host: 'sql5.freesqldatabase.com',
-    user: 'sql5390952',
-    password: 'wm3XCSHcM3',
-    database: 'sql5390952',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     multipleStatements: true
 })
 
